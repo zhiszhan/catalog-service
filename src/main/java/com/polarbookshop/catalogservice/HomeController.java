b094dfda76a43c1b29eb0ed58d75af7a025d100f
zhishou <zszhang@hotmail.com>
2024-04-27 07:40:39 +0000

    6th commit

--- src/main/java/com/polarbookshop/catalogservice/HomeController.java
+++ src/main/java/com/polarbookshop/catalogservice/HomeController.java
@@ -1,13 +1,26 @@
 package com.polarbookshop.catalogservice;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+//import com.polarbookshop.catalogservice.config.PolarProperties;
 
 @RestController
 public class HomeController {
 
+    @Value("${polar.greeting}")
+    private String greeting;
+    //private final PolarProperties polarProperties;
+
+    // public HomeController(PolarProperties polarProperties){
+    //     this.polarProperties = polarProperties;
+    // }
+
     @GetMapping("/")
     public String welcome() {
-        return "Welcome to the book catalog";
+        //return "Welcome to the book catalog";
+        return this.greeting;
+        //return polarProperties.getGreeting();
     }
 }
